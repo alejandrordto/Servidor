351b29deffbe0de99c9e33a43ef53cd1ababaa8b
alejandrordto <masterale2011@gmail.com>
2018-09-02 10:44:00 -0500

    pom

--- src/main/java/edu/escuelaing/arem/servidor.java
+++ src/main/java/edu/escuelaing/arem/servidor.java
@@ -7,8 +7,14 @@ public class servidor {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
+         Integer PORT;
+         try { 
+            PORT = new Integer(System.getenv("PORT"));
+        } catch (Exception e) {
+            PORT = 35000;
+        }
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(PORT);
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
