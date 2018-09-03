69c8d4d09a4f9e9b14c8a6914268c5c18ace5203
alejandrordto <masterale2011@gmail.com>
2018-09-03 08:56:02 -0500

    refactorizacion

---
+++ src/main/java/edu/escuelaing/arem/Carga.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author AsusPC
+ */
+public class Carga {
+
+    public void carga(Socket clientSocket) throws IOException {
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        String inputu = null;
+        while ((inputLine = in.readLine()) != null) {
+            try {
+                if (inputLine.startsWith("GET")) {
+                    if (!inputLine.startsWith("GET / ")) {
+                        inputu = inputLine;
+                    }
+                }
+            } catch (java.lang.StringIndexOutOfBoundsException e) {
+            }
+            //System.out.println(inputLine);
+            if (!in.ready()) {
+                break;
+            }
+        }
+        if (inputu != null) {
+            String[] temp;
+            temp = inputu.split(" ");
+            String flag = "";
+            if (temp[1].endsWith(".html")) {
+                File f = new File(temp[1].substring(1));
+                BufferedReader entrada;
+
+                try {
+                    entrada = new BufferedReader(new FileReader(f));
+                    while (entrada.ready()) {
+                        flag += entrada.readLine();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (temp[1].endsWith("png")) {
+                inputu = ("<!DOCTYPE html>"
+                        + "<html>"
+                        + "<head>"
+                        + "<title>Page Title</title>"
+                        + "</head>"
+                        + "<body>"
+                        + "<img src=\"" + temp[1].substring(1) + "\" height=\"200\" width=\"200\" >"
+                        + "</body>"
+                        + "</html>");
+            } else {
+                inputu = ("<!DOCTYPE html>"
+                        + "<html>"
+                        + "<head>"
+                        + "</head>"
+                        + "<body>"
+                        + "Por favor ingrese un recurso valido"
+                        + "</body>"
+                        + "</html>");
+            }
+            inputu = flag;
+        }
+        outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + inputu
+                + inputLine;
+        out.println(outputLine);
+        out.close();
+        clientSocket.close();
+    }
+
+}
--- src/main/java/edu/escuelaing/arem/servidor.java
+++ src/main/java/edu/escuelaing/arem/servidor.java
@@ -21,9 +21,8 @@ public class servidor {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
         }
-       Socket clientSocket = null;
+        Socket clientSocket = null;
         while (true) {
-            
             try {
                 System.out.println("Listo para recibir ...");
                 clientSocket = serverSocket.accept();
@@ -31,71 +30,9 @@ public class servidor {
                 System.err.println("Accept failed.");
                 System.exit(1);
             }
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String inputLine, outputLine;
-            String inputu = null;
-            while ((inputLine = in.readLine()) != null) {
-                try {
-                    if (inputLine.startsWith("GET")) {
-                        if (!inputLine.startsWith("GET / ")) {
-                            inputu = inputLine;
-                        }
-                    }
-                } catch (java.lang.StringIndexOutOfBoundsException e) {
-                }
-                //System.out.println(inputLine);
-                if (!in.ready()) {
-                    break;
-                }
-            }
-            if (inputu != null) {
-                String[] temp;
-                temp = inputu.split(" ");
-                String flag = "";
-                if (temp[1].endsWith(".html")) {
-                    File f = new File(temp[1].substring(1));
-                    BufferedReader entrada;
-
-                    try {
-                        entrada = new BufferedReader(new FileReader(f));
-                        while (entrada.ready()) {
-                            flag += entrada.readLine();
-                        }
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else if (temp[1].endsWith("png")){
-                     inputu=("<!DOCTYPE html>"
-                    +"<html>"
-                    +"<head>"
-                    +"<title>Page Title</title>"
-                    +"</head>"
-                    +"<body>"
-                    +"<img src=\""+ temp[1].substring(1) +"\" height=\"200\" width=\"200\" >"
-                    + "</body>"
-                    + "</html>");
-                } else {
-                    inputu=("<!DOCTYPE html>"
-                    +"<html>"
-                    +"<head>"
-                    +"</head>"
-                    +"<body>"
-                   +"Por favor ingrese un recurso valido"
-                    +"</body>"
-                    + "</html>");
-                }
-                inputu = flag;
-            }
-            outputLine = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: text/html\r\n"
-                    + "\r\n"
-                    + inputu
-                    + inputLine;
-            out.println(outputLine);
-            //out.close();
-            //*clientSocket.close();
+            Carga carga = new Carga();
+            carga.carga(clientSocket);
         }
+
     }
 }
